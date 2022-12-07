dc2d3b6ab9c33f5196dafab69014196250130feb
Pierre Guirand <p_guirand@hotmail.com>
2022-12-07 17:19:23 -0500

    new added

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 
-from .models import Flight, Airport, Passenger
+from .models import Airport, Flight, Passenger
 
 # Register your models here.
 
 class FlightAdmin(admin.ModelAdmin):
-    list_display = ("id","origin","destination","duration")
+    list_display = ("__str__", "duration")
 
 class PassengerAdmin(admin.ModelAdmin):
     filter_horizontal = ("flights",)
+    
 
 admin.site.register(Airport)
 admin.site.register(Flight, FlightAdmin)
